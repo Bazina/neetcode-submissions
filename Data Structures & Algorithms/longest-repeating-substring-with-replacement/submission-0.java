eb628d5d3da6b043b6fc582f9eba87c858e6c0f3
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2026-08-11 22:49:01 +0300

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int longest = 0;
+
+            HashMap<Character, Integer> freq = new HashMap<>();
+            char mostFrequentChar = ' ';
+            int mostFrequent = 0;
+
+            int l = 0;
+            for (int r = 0; r < s.length(); r++) {
+                char c = s.charAt(r);
+                freq.put(c, freq.getOrDefault(c, 0) + 1);
+
+                if (mostFrequent < freq.get(c)) {
+                    mostFrequentChar = c;
+                    mostFrequent = freq.get(c);
+                }
+
+                int windowLength = r - l + 1;
+                int numberOfReplacements = k - (windowLength - mostFrequent);
+
+                if (numberOfReplacements >= 0) {
+                    longest = Math.max(longest, windowLength);
+                } else {
+                    c = s.charAt(l++);
+                    freq.put(c, freq.get(c) - 1);
+
+                    if (mostFrequentChar == c) {
+                        mostFrequent--;
+                        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+                            if (mostFrequent < entry.getValue()) {
+                                mostFrequent = entry.getValue();
+                                mostFrequentChar = entry.getKey();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return longest;
+    }
+}
